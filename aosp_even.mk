b39e9c8fbbfc6085b1c69044db4a8a7a05cd6046
eun0115 <gianpaoloestacio5@gmail.com>
2022-03-11 08:33:06 +0800

    evem: Inherit ROM stuff
    Signed-off-by: eun0115 <gianpaoloestacio5@gmail.com>

--- aosp_even.mk
+++ aosp_even.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# Inherit some common Pixel-Experience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
