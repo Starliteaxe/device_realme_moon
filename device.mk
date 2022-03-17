7066d2a47fac8a627610452d802cf4ebe03bc352
Gian Paolo Estacio <gianpaoloestacio5@gmail.com>
2022-03-17 08:28:34 +0800

    even: Add RealmeParts
    Signed-off-by: Gian Paolo Estacio <gianpaoloestacio5@gmail.com>

--- device.mk
+++ device.mk
@@ -25,6 +25,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # IMS
 $(call inherit-product, vendor/realme/even-ims/even-ims.mk)
 
+# RealmeParts
+$(call inherit-product, packages/apps/RealmeParts/parts.mk)
+
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Dynamic Partition
